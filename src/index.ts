5950d1ed1b3d0408865cede785da0a792c61cd64
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-05 19:38:17 +0000

    Refactored index.ts to use socket.io config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-﻿import * as app from './config/express';
+﻿import * as http from './config/socket.io';
 import * as config from './config/config';
 
-app.listen(config.port, () => {
+http.listen(config.port, () => {
   console.log(`Server started on port ${config.port} (${config.env})`);
 });
 
-export = app;
+export = http;
