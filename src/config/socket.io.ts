30661aab62b595b1ef02051aad4ed98ad1e907ed
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-06 07:16:38 +0000

    Refactored Socket.io config

--- src/config/socket.io.ts
+++ src/config/socket.io.ts
@@ -1,5 +1,6 @@
 import * as app from './express';
-var http = require('http').Server(app);
-let io = require('socket.io')(http);
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 export = http;
