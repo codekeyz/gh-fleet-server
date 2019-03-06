7b4e60994ed9d973b7459af421386ae92b5066a9
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-06 09:41:17 +0000

    Added scaffolding for socket.io config

--- src/config/socket.io.ts
+++ src/config/socket.io.ts
@@ -3,4 +3,10 @@ import * as app from './express';
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+io.on('connection', socket => {
+
+    socket.in('').
+
+});
+
 export = http;
