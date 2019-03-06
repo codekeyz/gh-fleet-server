f2dd5c4cade835fd416a1569aa6164901416e4e2
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-06 07:49:44 +0000

    Loaded version from package.json file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,11 @@
 import Joi = require('joi');
+import {number} from "joi";
+const packJson = require('../../package.json');
 
 // require and configure dotenv, will load vars in .env in PROCESS.ENV
 require('dotenv').config();
+
+
 
 // define validation for all the env vars
 const envVarsSchema = Joi.object({
@@ -12,7 +16,8 @@ const envVarsSchema = Joi.object({
   MARIADB_HOST: Joi.string()
     .required()
     .description('Maria DB host url'),
-  MARIADB_PORT: Joi.number().default(3306)
+  MARIADB_PORT: Joi.number().default(3306),
+  API_VERSION: Joi.number().default(parseFloat(packJson.version).toPrecision(2))
 })
   .unknown()
   .required();
@@ -26,6 +31,7 @@ const config = {
   env: envVars.NODE_ENV,
   port: envVars.SERVER_PORT,
   jwtSecret: envVars.JWT_SECRET,
+  version: envVars.API_VERSION,
   mariadb: {
     host: envVars.MARIADB_HOST,
     port: envVars.MARIADB_PORT
