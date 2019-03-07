13705d4b11d37174740e06642b97c876c9102187
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-07 22:22:54 +0000

    Added sanitizer

---
+++ src/resources/sanitizers.ts
@@ -0,0 +1,11 @@
+import {IUser} from '../models/user.model';
+
+export function sanitizeUserData(data: IUser) {
+    return {
+        id: data._id,
+        username: data.username,
+        email: data.email,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt
+    }
+}
