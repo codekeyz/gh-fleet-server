0403cf5f678e25a3cdb80ab9db12fcd3d988ba68
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-07 12:48:57 +0000

    Added user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,39 @@
+import mongoose = require('mongoose');
+
+const userschema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
+        match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    roles: [{
+        type: String,
+    }]
+}, {
+    versionKey: false
+});
+
+export = mongoose.model('User', userschema);
