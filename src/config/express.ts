5798bf2fc91d88bec321a123e948d45a7518eea8
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-07 12:47:36 +0000

    Added Exprss COnfig

--- src/config/express.ts
+++ src/config/express.ts
@@ -7,14 +7,16 @@ import cors = require('cors');
 import bodyParser = require('body-parser');
 import * as config from './config';
 import * as routes from '../routes/index.routes';
+import * as passport from '../config/passport';
+
 const app = express();
 
 if (config.env === 'development') {
-  app.use(logger('dev'));
+    app.use(logger('dev'));
 }
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(compress());
 
@@ -24,29 +26,30 @@ app.use(helmet());
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
 
-console.log(config.version);
+// Setup passport
+app.use(passport.initialize());
 
 // API router
 app.use(`/api/v${config.version}`, routes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = httpError(404);
-  return next(err);
+    const err = httpError(404);
+    return next(err);
 });
 
 // error handler, send stacktrace only during development
 app.use((err, req, res, next) => {
-  // customize Joi validation errors
-  if (err.isJoi) {
-    err.message = err.details.map(e => e.message).join('; ');
-    err.status = 400;
-  }
+    // customize Joi validation errors
+    if (err.isJoi) {
+        err.message = err.details.map(e => e.message).join('; ');
+        err.status = 400;
+    }
 
-  res.status(err.status || 500).json({
-    message: err.message
-  });
-  next(err);
+    res.status(err.status || 500).json({
+        message: err.message
+    });
+    next(err);
 });
 
 export = app;
