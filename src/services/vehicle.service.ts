f7de9bf0c40c7d85ce9da588d367841d3397505b
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-08 00:50:22 +0000

    Configured Vehicle Service

--- src/services/vehicle.service.ts
+++ src/services/vehicle.service.ts
@@ -1,12 +1,19 @@
 import {injectable} from 'inversify';
+import {IVehicle, vehicleModel as Vehicle} from '../models/vehicle.model';
 
 
 @injectable()
 export class VehicleService {
 
+    public getVehicleByOwnerId = function (owner: string) {
+        return Vehicle.find({owner})
+            .populate('owner')
+            .exec();
+    };
 
-    public getUsers() {
-
+    public createVehicle = function (data: IVehicle) {
+        let vh = new Vehicle(data);
+        return vh.save()
     }
 }
 
