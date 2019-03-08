86def318287d8917f4bd4d13dbdbf30f94b9ef30
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-08 18:25:46 +0000

    Fixed main entry file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 ﻿import * as http from './config/socket.io';
 import * as config from './config/config';
-import './config/mongoose';
 
 http.listen(config.port, () => {
     console.log(`Server started on ${config.port} (${config.env})`);
