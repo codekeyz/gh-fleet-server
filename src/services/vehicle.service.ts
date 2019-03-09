a01613d56160e3a285578fb27d549d316d76b8fc
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-09 23:54:39 +0000

    Added some functions to vehicle service

--- src/services/vehicle.service.ts
+++ src/services/vehicle.service.ts
@@ -1,17 +1,32 @@
 import {injectable} from 'inversify';
 import {IVehicle, vehicleModel as Vehicle} from '../models/vehicle.model';
+import {IUser} from '../models/user.model';
 
 
 @injectable()
 export class VehicleService {
 
-    public getVehicles = function (options: {}) {
+    public find = function (_id?, owner?) {
+        let options = {};
+        if (owner){
+            options['owner'] = owner;
+        }
+        if (_id) {
+            options['_id'] = _id;
+        }
+        return Vehicle.find(options);
+    };
 
-    }
-
-    public createVehicle = function (data: IVehicle) {
+    public createVehicle = function (user: IUser, data: IVehicle) {
+        data.owner = user;
         let vh = new Vehicle(data);
         return vh.save()
+    }
+
+    public updateVehicle = function (query: {}, update: {}) {
+        return Vehicle.findOneAndUpdate(query, update, {
+            new: true,
+        });
     }
 }
 
