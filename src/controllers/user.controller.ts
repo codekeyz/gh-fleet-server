4d8af56bf7c7a40f9f9ae6e0171af19b13a764a4
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-09 23:56:45 +0000

    Fixed user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,15 +1,14 @@
 import {controller, httpGet, httpPost, httpPut, interfaces, requestParam} from 'inversify-express-utils';
 import {inject} from 'inversify';
 import {UserService} from '../services/user.service';
-import {Request, Response} from 'express';
+import {NextFunction, Request, Response} from 'express';
 import TYPES from '../config/di/types';
-import {check, validationResult} from 'express-validator/check';
+import {body, check, validationResult} from 'express-validator/check';
 import {VehicleService} from '../services/vehicle.service';
 import {FirebaseService} from '../services/firebase.service';
+import '../typings/express.user.module';
 import userResource = require('../resources/user.resource');
 import vehicleResource = require('../resources/vehicle.resource');
-import '../typings/express.user.module';
-
 
 @controller('/users')
 export class UserController implements interfaces.Controller {
@@ -45,14 +44,13 @@ export class UserController implements interfaces.Controller {
         }
 
         try {
-            const acc = await this._firebSvc.getAuth().createUser({
+            const result = await this._userSvc.createUser(req.body);
+            await this._firebSvc.getAuth().createUser({
+                uid: result.id,
                 email: req.body.email,
-                // @ts-ignore
                 password: req.body.password,
                 displayName: req.body.username
             });
-            req.body._id = acc.uid;
-            const result = await this._userSvc.createUser(req.body);
             return res.status(200).json(userResource.single(result));
         }
         catch (err) {
@@ -60,7 +58,7 @@ export class UserController implements interfaces.Controller {
         }
     }
 
-    @httpGet('/me',
+     @httpGet('/me',
         TYPES.UserLoggedInMiddleWare
     )
     public getMyAccount(req: Request, res: Response) {
@@ -72,16 +70,78 @@ export class UserController implements interfaces.Controller {
     )
     public async postVehicle(req: Request, res: Response) {
         const user = req.user;
-        let vh = await this._vhSvc.createVehicle(req.body);
+        let vh = await this._vhSvc.createVehicle(user, req.body);
         user.vehicles.push(vh);
-        const result = await user.save();
-        return res.json(await vehicleResource.collection(result.vehicles));
+        await user.save();
+        return res.json(await vehicleResource.single(vh));
     }
 
     @httpPut('/me/vehicles/:id',
-        TYPES.UserLoggedInMiddleWare)
-    public async updateVehicle(@requestParam("id") id: string, res: Response) {
-            return res.send(id);
+        TYPES.UserLoggedInMiddleWare,
+        body('name')
+            .optional()
+            .not().isEmpty()
+            .trim(),
+        body('color')
+            .optional()
+            .not().isEmpty()
+            .trim(),
+        body('vehicle_type_name')
+            .optional()
+            .not().isEmpty()
+            .trim(),
+        check('fuel_volume_units')
+            .optional()
+            .not().isEmpty()
+            .isIn(['us_gallons', 'uk_gallons', 'litres'])
+            .withMessage('fuel_volume_units must be one of this [us_gallons, uk_gallons, litres]'),
+        check('archived')
+            .optional()
+            .not().isEmpty()
+            .isBoolean()
+            .withMessage('archived field requires a boolean')
+    )
+    public updateVehicle(@requestParam("id") _id: string, req: Request, res: Response, next: NextFunction) {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array()});
+        }
+
+        let query = {};
+        let update = {};
+        query['_id'] = _id;
+        query['owner'] = req.user.id;
+
+        let name = req.body.name;
+        let color = req.body.color;
+        let volume = req.body.fuel_volume_units;
+        let vtypename = req.body.vehicle_type_name;
+        let archived: boolean = req.body.archived;
+
+        if (name) {
+            update['name'] = name;
+        }
+        if (color) {
+            update['color'] = color;
+        }
+        if (volume) {
+            update['fuel_volume_units'] = volume;
+        }
+        if (vtypename) {
+            update['vehicle_type_name'] = vtypename;
+        }
+        if (archived) {
+            update['archived'] = archived;
+        }
+
+        return this._vhSvc.updateVehicle(query, {
+            $set: update
+        }).then(result => {
+            res.send(vehicleResource.single(result));
+        }).catch(err => {
+            res.send(err.message);
+        });
     }
 
     @httpGet('/me/vehicles',
