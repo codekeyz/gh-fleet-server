77378f9b7282239ed9a0af70e4190c30ba82b03b
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-09 23:55:28 +0000

    Refactored Vehicle Resource

--- src/resources/vehicle.resource.ts
+++ src/resources/vehicle.resource.ts
@@ -7,16 +7,22 @@ class VehicleResource implements BaseSanitizer<IVehicle> {
         return this.handleCollection(datalist);
     }
 
-    public single(data: IVehicle, wrap?: boolean): {} {
+    public single(data: IVehicle, wrap = true): {} {
         let result =
             {
                 id: data._id,
                 name: data.name,
                 color: data.color,
+                archived: data.archived,
                 license_plate: data.license_plate,
                 fuel_volume_units: data.fuel_volume_units,
                 vehicle_type_model: data.vehicle_type_model,
                 vehicle_type_name: data.vehicle_type_name,
+                owner: {
+                    username: data.owner.username,
+                    email: data.owner.email,
+                    telephone: data.owner.telephone
+                },
                 createdAt: data.createdAt,
                 updatedAt: data.updatedAt,
             };
@@ -26,7 +32,7 @@ class VehicleResource implements BaseSanitizer<IVehicle> {
     private async handleCollection(datalist: IVehicle[]) {
         let result = [];
         let opts = datalist.map(vehicle => {
-            result.push(this.single(vehicle))
+            result.push(this.single(vehicle, false))
         });
         await Promise.all(opts);
         return {
