382b098ae8c9bfdc46a0d39a91f8b8da6164623c
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-11 14:20:33 +0000

    Added query support to find vehicles

--- src/services/vehicle.service.ts
+++ src/services/vehicle.service.ts
@@ -6,13 +6,22 @@ import {IUser} from '../models/user.model';
 @injectable()
 export class VehicleService {
 
-    public find = function (_id?, owner?) {
-        let options = {};
+    public find = function (_id?, owner?, color?, fuel_volume_units?, vehicle_type_name?, archived = false) {
+        let options = {archived};
         if (owner) {
-            options['owner'] = owner;
+            options['owner'] = {'$regex': owner, $options: 'i'};
+        }
+        if (color) {
+            options['color'] = {'$regex': color, $options: 'i'};
         }
         if (_id) {
             options['_id'] = _id;
+        }
+        if (fuel_volume_units) {
+            options['fuel_volume_units'] = {'$regex': fuel_volume_units, $options: 'i'};
+        }
+        if (vehicle_type_name) {
+            options['vehicle_type_name'] = {'$regex': vehicle_type_name, $options: 'i'};
         }
         return Vehicle.find(options);
     };
