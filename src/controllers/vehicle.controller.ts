9fa262f9674a62636aa719339035268a26068e24
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-11 14:19:16 +0000

    Added Get Vehicles with Query function

--- src/controllers/vehicle.controller.ts
+++ src/controllers/vehicle.controller.ts
@@ -1,16 +1,37 @@
-import {controller, httpGet} from 'inversify-express-utils';
+import {controller, httpGet, queryParam} from 'inversify-express-utils';
 import {inject} from 'inversify';
 import TYPES from '../config/di/types';
-import {NextFunction, Request, Response} from 'express';
+import {Request, Response} from 'express';
 import {VehicleService} from '../services/vehicle.service';
+import vehicleResource = require('../resources/vehicle.resource');
+import {number} from 'joi';
 
 @controller('/vehicles')
 export class VehicleController {
 
-    constructor(@inject(TYPES.VehicleService) private vehicleSvc: VehicleService) {}
+    constructor(@inject(TYPES.VehicleService) private _vhSvc: VehicleService) {
+    }
 
     @httpGet('/')
-    private index(req: Request, res: Response, next: NextFunction): string {
-        return 'Hello world';
+    private async index(
+        @queryParam('color') color: string,
+        @queryParam('fuel_volume_units') fuel_volume_units: string,
+        @queryParam('vehicle_type_name') vehicle_type_name: string,
+        @queryParam('archived') archived: boolean,
+        @queryParam('offset') offset = `${0}`,
+        @queryParam('limit') limit = `${50}`,
+        req: Request,
+        res: Response) {
+        try {
+            const result = await this._vhSvc.find(null, null, color, fuel_volume_units, vehicle_type_name, archived)
+                .skip(parseInt(offset))
+                .limit(parseInt(limit))
+                .populate('owner')
+                .exec();
+            const formattedresult = await vehicleResource.collection(result);
+            res.send(formattedresult);
+        } catch (e) {
+            res.status(400).send(e);
+        }
     }
 }
