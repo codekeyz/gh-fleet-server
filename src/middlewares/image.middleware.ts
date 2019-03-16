1baa5f5de186ed831c8df1e12d03cb28c194ef8d
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-16 09:47:07 +0000

    Refactored Image Middleware

--- src/middlewares/image.middleware.ts
+++ src/middlewares/image.middleware.ts
@@ -1,7 +1,6 @@
 import {injectable} from 'inversify';
 import {BaseMiddleware} from 'inversify-express-utils';
 import {NextFunction, Request, Response} from 'express';
-import fileType from 'file-type';
 
 export const accepted_extensions = ['jpg', 'png'];
 
@@ -12,18 +11,18 @@ export class ImageMiddleware extends BaseMiddleware {
     handler(req: Request, res: Response, next: NextFunction): void {
 
         // if the file is not present
-        if (!req.file)
-            return next(new Error('No file found in request body'));
+        if (!req.file || !req.files)
+            return next(new Error('No file/files found in request body'));
 
 
         // For MemoryStorage, validate the format using `req.file.buffer`
         // For DiskStorage, validate the format using `fs.readFile(req.file.path)` from Node.js File System Module
-        let mime = fileType(req.file.buffer);
+        // let mime = fileType(req.file.buffer);
 
         // if can't be determined or format not accepted
         // @ts-ignore
-        if (!mime || !accepted_extensions.includes(mime.ext))
-            return next(new Error('The uploaded file is not in ' + accepted_extensions.join(", ") + ' format!'));
+        // if (!mime || !accepted_extensions.includes(mime.ext))
+        //     return next(new Error('The uploaded file is not in ' + accepted_extensions.join(", ") + ' format!'));
 
         next();
     }
